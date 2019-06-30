2c0f552ccce75612cd2202c27b30d7d4c3458381
Younes Henni <y.henni@fairfx.com>
2019-06-30 19:08:12 +0100

    First commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity>
+        <Text>Press me.</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
